498b43253b5c232380315264e3322ba9186a44e1
Stefanos Cloud <stefanoscloud@protonmail.com>
2024-04-20 11:59:07 +0300

    Create spaCy-snippets.py

---
+++ ML/spaCy/spaCy-snippets.py
@@ -0,0 +1,9 @@
+# SpaCy Python library
+
+#Text processing
+# nlp= spacy.load(‘package’) – Load a spaCy model with the specified package name, to use with your text.
+# Doc= nlp(text) – Create a spaCy Language object out of an object containing text.
+# Doc.sents – Create an iterable object for extracting the sentences (sents) in the doc object.
+# Sentence.text – Create an iterable object that tokenizes a sentence object into words.
+# Token.is_stop – Determine whether a tokenized word is considered a stop word.
+# Token.lemma_  - Retrieve the lemma of each tokenized word.
