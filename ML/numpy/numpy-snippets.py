ca1e9a22ef252aa3092967e2e3c6d33387a7b020
Stefanos Cloud <stefanoscloud@protonmail.com>
2024-04-20 12:06:18 +0300

    Update numpy-snippets.py

--- ML/numpy/numpy-snippets.py
+++ ML/numpy/numpy-snippets.py
@@ -3,3 +3,7 @@
 #Image processing
 # Numpy.fliplr(image_gray) – Flip  image horizontally
 # Numpy.flipud(image_gray) – Flip image vertically
+
+#Audio processing
+#Numpy.append(samples, np-zeros(24000)) – Add silence (zero amplitude) to the end of the of the audio track. 
+
