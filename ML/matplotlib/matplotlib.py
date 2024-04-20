691c8fbe3c89b56f255721cf086786c39bf4e3b7
Stefanos Cloud <stefanoscloud@protonmail.com>
2024-04-20 12:01:51 +0300

    Create matplotlib.py

---
+++ ML/matplotlib/matplotlib.py
@@ -0,0 +1,6 @@
+#Matplotlib Python library
+
+#Image processing
+# Image= matplotlib.image.imread(‘image.jpg’) – Method for reading an image.
+# Matplotlib.pyplot.imshow(image_gray, cmap=‘gray’) – Plot the image using a grayscale color map.
+# Matplotlib.image.imsave(‘image_gray.jpg’, image_gray, cmap=‘gray’) – Save an image to local storage using grayscale map
