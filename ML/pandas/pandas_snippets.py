7f7134323901a012dfbd7682281229b338eb6efa
Stefanos Cloud <stefanoscloud@protonmail.com>
2024-04-20 11:53:31 +0300

    Create pandas_snippets.py

---
+++ ML/pandas/pandas_snippets.py
@@ -0,0 +1,6 @@
+# Pandas dataframe() class
+# df= pandas.read_csv(‘path to csv’) – Pass in the path of a CSV to load it into the Python environment as DF
+# df= pandas.read_excel(‘path to excel’) – Pass in the path of an Excel to load it into the Python environment as DF
+# df= pandas.read_pickle(‘path to pickle’) – Pass in the path of a pickle file to load it into the Python environment as DF
+# df.info() – Retries various attributes of the data frame, including the number of entries (rows), number of columns, the name of each column and the data type of each column.
+# df.head() – Return the first five rows of a data frame.
