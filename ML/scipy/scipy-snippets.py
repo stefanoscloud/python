c7ebb38b9425d3f6e095125eacbca91e73e02362
Stefanos Cloud <stefanoscloud@protonmail.com>
2024-04-20 12:35:13 +0300

    Create scipy-snippets.py

---
+++ ML/scipy/scipy-snippets.py
@@ -0,0 +1,5 @@
+# SciPy library for Python
+
+# Hierarchical clustering models - Dendrogram analysis - hierarchy module
+# linkage = scipy.cluster.hierarchy.linkage(X_train, method = 'ward') —This constructs a linkage object to use with the dendrogram.
+# scipy.cluster.hierarchy.dendrogram(Z = linkage) —This constructs a dendrogram based on the provided linkage object.
