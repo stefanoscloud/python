6e51e281510b0786d6b99aa5619998d33b0e4e4e
Stefanos Cloud <stefanoscloud@protonmail.com>
2024-04-20 12:09:24 +0300

    Update scikit-learn-snippets.py

--- ML/scikit-learn/scikit-learn-snippets.py
+++ ML/scikit-learn/scikit-learn-snippets.py
@@ -12,3 +12,14 @@
 #Train ML models
 # sklearn.model_selection module with the sklearn.model_selection.train_test_split() 
 # X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.7, random_state = 1)
+
+#Evaluate and tune ML models
+# Kf= sklearn.model_selection.Kfold(n_splits= 5, shuffle = True) – Creates a cross-validation object that will split the training data into five folds. The shuffle=True argument indicates that the data will be shuffled randomly before being split. 
+#Kf.split(X) – Uses the kf object to perform cross-validation on the X dataset, returning the indices with which to split the data into train and test subsets. 
+#Skf= sklearn.model_selection.StratifiedKFold(n_splits= 5, shuffle= True) – Creates an object that will return stratified folds based on the percentages of each class provided as labels. The data examples from each class will be shuffled before splitting. 
+#Skf.split(X, y) – Performs cross-validation on the X data, while using the data in y as the class labels for stratification. 
+#Lpocv= sklearn.model_selection.LeavePOut(p=3) – Creates a cross-validation object using LPOCV, w ith number of examples in test=3
+#Loocv= sklearn.model_selection.LeaveOneOut() – Same as sklearn.model_selection.LeavePOut(p=1) 
+
+# Cv= sklearn.model_selection.cross_validate(estimator, X, y, cv=5) - This performs splitting, fitting and evaluation in one step, by running cross validation
+
