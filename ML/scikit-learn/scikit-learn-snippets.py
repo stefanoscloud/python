7d0d329fe7c5b6ac31b2ac575f3451cab32a2256
Stefanos Cloud <stefanoscloud@protonmail.com>
2024-04-20 12:07:29 +0300

    Update scikit-learn-snippets.py

--- ML/scikit-learn/scikit-learn-snippets.py
+++ ML/scikit-learn/scikit-learn-snippets.py
@@ -8,3 +8,7 @@
 # binner.fit_transform(data) – Return the binned values of all input data based on an existing binner object.
 # pca= sklearn.decomposition.PCA(n_components = 2) – Create an object that will perform principal component analysis (PCA) on a dataset and return two features.
 # Pca.fit_transform(data) – Return the reduced feature values from the input data, based on an existing pca object.
+
+#Train ML models
+# sklearn.model_selection module with the sklearn.model_selection.train_test_split() 
+# X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.7, random_state = 1)
