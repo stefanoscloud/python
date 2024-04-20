f0f3af1b0114b4914b0a0cab46f27be70eb4694c
Stefanos Cloud <stefanoscloud@protonmail.com>
2024-04-20 12:30:38 +0300

    Update scikit-learn-snippets.py

--- ML/scikit-learn/scikit-learn-snippets.py
+++ ML/scikit-learn/scikit-learn-snippets.py
@@ -43,5 +43,53 @@
 # Model= sklearn.linear_model.SGDRegressor(penalty= ‘l2’, alpha= 0.1, learning_rate= ‘constant’, eta0 = 0.05) 
 # Use these classes to call the same methods and attributes as in LinearRegression() class 
 
+# Logistic regression for training binary classification models - LogisticRegression() class 
+# model = sklearn.linear_model.LogisticRegression(penalty = 'l2', C = 0.05, solver = 'sag') —This constructs a model object that uses the logistic regression algorithm. In this case, the model is using regularization and an iterative approach to minimizing cost.
+# model.fit(X_train, y_train) —Fit a set of training data to the model.
+# model.score(X_test, y_test) —Return the accuracy score for the model given validation/test data.
+# model.predict(X_test) —Use the model to return estimated classifications on the validation/test set.
+# model.predict_proba(X_test) —Use the model to provide the raw probability estimates for each classification decision.
+# model.coef_ —Return an attribute that lists the optimal model parameters generated during training.
+
+# K-NN for training binary classification models - KNeighborsClassifier() class 
+# model = sklearn.neighbors.KNeighborsClassifier(n_neighbors = 3) —This constructs a model object that uses the k-NN algorithm. In this case, k is 3.
+# You can use this class object to call the same fit(), score(), predict(), and predict_proba() methods as with LogisticRegression(). However, predict_proba() will return the fraction of neighbors that voted for each label, rather than a true prediction probability.
+
+# Logistic regression for training multiclass classification models - LogisticRegression() class 
+#model = sklearn.linear_model.LogisticRegression(multi_class = 'multinomial’) —This constructs a model object that uses the logistic regression algorithm. In this case, the model is enabling multi-class classification through the use of a multinomial logistic algorithm.
+#You can use this class object to call the same methods and return the same attributes mentioned previously, i.e. fit(), score(), predict(), and predict_proba().
+
+# Evaluate a classification model - Metrics module
+# sklearn.metrics.confusion_matrix(y_test, prediction) —Return the confusion matrix for the model given validation/test data.
+# sklearn.metrics.accuracy_score(y_test, prediction) —Return the accuracy score for the model given validation/test data.
+# sklearn.metrics.precision_score(y_test, prediction) —Return the precision score for the model given validation/test data.
+# sklearn.metrics.recall_score(y_test, prediction) —Return the recall score for the model given validation/test data.
+# sklearn.metrics.f1_score(y_test, prediction) —Return the F1 score for the model given validation/test data.
+# sklearn.metrics.roc_curve(y_test, prediction_proba) —Return the ROC curve for the model given validation/test data.
+# sklearn.metrics.roc_auc_score(y_test, prediction_proba) —Return the ROC AUC score for the model given validation/test data.
+# sklearn.metrics.precision_recall_curve(y_test, prediction_proba) —Return the PRC for the model given validation/test data.
+# sklearn.metrics.average_precision_score(y_test, prediction_proba) —Return the average precision score for the model given validation/test data
+# The prediction argument is an object created by calling predict() on the model, whereas prediction_proba is the same, but for calling predict_proba(). 
+
+# Tune a classification model (hyperparameter optimization) - GridSearchCV() and RandomizedSearchCV() classes 
+# search = sklearn.model_selection.GridSearchCV(model, param_grid = grid, scoring = 'recall', cv = 5) —This constructs a grid search object for the given machine learning model, using the provided parameter grid. In this case, the search will optimize based on recall, and will perform five-fold cross-validation.
+# search = sklearn.model_selection.RandomizedSearchCV(model, param_distributions = dist, n_iter = 100, scoring = 'recall', cv = 5) —This constructs a grid search object for the given machine learning model. It takes a parameter distribution and the number of iterations with which to try the hyperparameter combinations. 
+
+# Build K-Means clustering models - KMeans() class 
+# model = sklearn.cluster.KMeans(n_clusters = 3) —This constructs a model object that uses the k-means clustering algorithm. In this instance, the number of clusters specified is 3.
+# model.fit(X_train) —Fit a set of training data to the model.
+# model.fit_predict(X_train) —Fit a set of training data to the model, and return what cluster each example belongs to.
+# model.predict(X_test) —Predict the cluster that each data example in a test set belongs to.
+# sklearn.metrics.silhouette_score(X_train, clusters) —Return the mean silhouette score for all examples. Note that clusters is an object created by calling fit_predict() on the model.
+# sklearn.metrics.silhouette_samples(X_train, clusters) —Return the silhouette score for all individual examples.
+
+
+
+
+
+
+
+
+
 
 
